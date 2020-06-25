953e236a3c31b0129f95bcf37a1b8b53cc3c3c78
Maja <maja.mihajlovic9@gmail.com>
2020-06-25 12:21:10 +0200

    add all files

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+let { MONGO_URI } = require("./keys");
+const PORT = 5000;
+
+require("./models/user");
+require("./models/post");
+
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("connected to the mongo");
+})
+
+mongoose.connection.on("error", () => {
+    console.log("error connecting");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on", PORT);
+})
---
+++ server/routes/post.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const requiredLogin = require("../middleware/requireLogin");
+const Post = mongoose.model("Post");
+
+router.get("/allpost", (req, res) => {
+    Post.find()
+        .populate("postedBy", "_id name")
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(err => console.log(err))
+})
+
+
+router.get("/mypost", requiredLogin, (req, res) => {
+    Post.find({ postedBy: req.user._id })
+        .populate("postedBy", "_id name")
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(err => console.log(err))
+})
+router.post("/createpost", requiredLogin, (req, res) => {
+    const { title, body, url} = req.body;
+
+    if (!title || !body || !url) {
+        res.status(422).json({ error: "Please fill all the required fields." })
+    }
+
+    req.user.password = undefined; //TODO
+
+    const post = new Post({
+        title,
+        body,
+        photo:url,
+        postedBy: req.user
+    })
+
+    post.save()
+        .then(response => {
+            console.log(res)
+            res.json({ post: response })
+        })
+        .catch(err => console.log(err))
+});
+
+module.exports = router;
