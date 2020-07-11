284d4cb52ab3343d9b45d1294bd822c3300b8a69
Maja <maja.mihajlovic9@gmail.com>
2020-07-11 21:00:38 +0200

    add client static files

---
+++ server/client/build/precache-manifest.af9e45a62f7a1e36ea36f6140e267c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f140ca2f438448b1c1d78343557b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b7a82dedc75c1f0658",
+    "url": "/static/css/main.ddc03bb9.chunk.css"
+  },
+  {
+    "revision": "2b8db614d477885f6e09",
+    "url": "/static/js/2.e2cc10d6.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.e2cc10d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b7a82dedc75c1f0658",
+    "url": "/static/js/main.d6ac3d5d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
